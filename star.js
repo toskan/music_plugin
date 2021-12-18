237f2447b452eab5d0a95f080c39c87f8cb76c68
admin <admin@admins-MacBook-Pro.local>
2021-12-18 13:49:55 -0500

    both  mutation observer functions

--- star.js
+++ star.js
@@ -1,4 +1,6 @@
-const observer = new MutationObserver((mutationList) => {
+const dropTable = document.querySelector('#drop_table tbody');
+
+const starObserver = new MutationObserver((mutationList) => {
 	mutationList.forEach(({ target, oldValue }) => {
 		if (target.src.includes('favorited') && oldValue.includes('empty')) {
 			let parent = target.parentNode.parentNode.parentNode.parentNode;
@@ -6,24 +8,50 @@ const observer = new MutationObserver((mutationList) => {
 			let artist =
 				parent.querySelectorAll('.col_artist font')[0].innerText;
 
-			let song = parent.querySelectorAll('.col_song_title font')[0]
+			let track = parent.querySelectorAll('.col_song_title font')[0]
 				.innerText;
 
 			let album = parent.querySelectorAll('.col_album_title font')[0]
 				.innerText;
 
 			console.log(
-				`Favorited Song: artist: ${artist} title: ${song} album: ${album}`
+				`Favorited Song: artist: ${artist} title: ${track} album: ${album}`
 			);
 		}
 	});
 });
 
-const dropTable = document.querySelector('#drop_table tbody');
-
-observer.observe(dropTable, {
+starObserver.observe(dropTable, {
 	attribute: true,
 	attributeFilter: ['src'],
 	subtree: true,
 	attributeOldValue: true,
 });
+
+//Nathan's code to collect currently playing info
+const observerLive = new MutationObserver((mutationList) => {
+	mutationList.forEach(({ addedNodes }) => {
+		addedNodes.forEach((node) => {
+			if (!/^drop_/.test(node.id)) {
+				return true;
+			}
+
+			const artist = getValueByClass(node, 'col_artist');
+			const track = getValueByClass(node, 'col_song_title');
+			const album = getValueByClass(node, 'col_album_title');
+			console.log('NEW!', { artist, track, album });
+		});
+	});
+});
+
+observerLive.observe(dropTable, {
+	childList: true,
+});
+
+function getValueByClass(node, klass) {
+	const child = Array.prototype.find.call(node.children, (child) => {
+		return child.classList.contains(klass);
+	});
+
+	return child?.children?.[0]?.innerText;
+}
