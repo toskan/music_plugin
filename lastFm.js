7a2af4a7749f376b1eafba41de02319b5eaab29f
admin <admin@admins-MacBook-Pro.local>
2021-12-21 17:24:40 -0500

    deleted lastFm

--- lastFm.js
+++
@@ -1,8 +0,0 @@
-const getToken = () => {
-	let urlString = window.location.href;
-	var url = new URL(urlString);
-	let token = url.searchParams.get('token');
-	chrome.storage.sync.set({ token });
-};
-
-getToken();
